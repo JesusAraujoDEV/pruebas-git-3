c55361094f0afaaa2fa60682868e6ce8c3344a47
Jesús Araujo <j2a0a0a5@gmail.com>
2025-07-25 10:33:50 -0400

    Implement initial project structure and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepositorio",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Jesús Araujo",                         # Tu nombre
+    author_email="jesusaraujodev@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/JesusAraujoDEV/pruebas-git-3",     # URL del proyecto
+)
